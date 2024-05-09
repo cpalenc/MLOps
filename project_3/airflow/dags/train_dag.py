4d62c2c7ba0390af8c0a7bd9282fe586a577d6d6
cpalenc <cpalenc@bancodebogota.com.co>
2024-05-08 23:42:24 -0500

    solve error track model

--- project_3/airflow/dags/train_dag.py
+++ project_3/airflow/dags/train_dag.py
@@ -42,8 +42,8 @@ def preprocess_data():
     df = pd.read_sql(query, con=mysql_engine)
 
     df = df.loc[:,['age', 'discharge_disposition_id', 'time_in_hospital', 'num_lab_procedures', 
-                   'num_procedures', 'number_inpatient', 'diag_1', 'diag_2', 'diag_3', 'number_diagnoses', 
-                   'readmitted']]
+                'num_procedures', 'number_inpatient', 'diag_1', 'diag_2', 'diag_3', 'number_diagnoses', 
+                'readmitted']]
 
     # Separar características (X) y variable objetivo (y)
     X = df.drop(['readmitted'], axis=1)
@@ -63,11 +63,15 @@ def preprocess_data():
     X_train, X_test, y_train, y_test = train_test_split(X_resampled, y_resampled, test_size=0.2, random_state=42)
 
     mlflow.set_tracking_uri("http://mlflow:5000")
+    #EXPERIMENT_NAME = "Readmitted-Survived-Classifier-Experiment-1"
     mlflow.set_experiment("diabetes-1")
-    mlflow.autolog(log_input_examples=True, log_model_signatures=True)
+    mlflow.sklearn.autolog(log_input_examples=True, log_model_signatures=True, registered_model_name='modelo_test_4')
 
     current_experiment = dict(mlflow.get_experiment_by_name('diabetes-1'))
     experiment_id = current_experiment['experiment_id']
+
+
+
 
     # Modelo Decision Tree
     model_name = 'Decision Tree'
@@ -79,18 +83,6 @@ def preprocess_data():
         predictions = model.predict(X_test)
         accuracy = accuracy_score(y_test, predictions)
         f1 = f1_score(y_test, predictions, average='weighted')  
-        mlflow.log_params(params)
-        mlflow.log_metric(f"{model_name}_accuracy", accuracy)
-        mlflow.log_metric(f"{model_name}_f1", f1)
-        mlflow.set_tag("Training Info", f"{model_name} model for Readmitted")
-        signature = infer_signature(X_train, model.predict(X_train))
-        model_info = mlflow.sklearn.log_model(sk_model=model, artifact_path=f"readmitted_{model_name}_model",
-                                              signature=signature, input_example=X_train,
-                                              registered_model_name=f"tracking-readmitted-{model_name}")
-        mlflow.end_run() 
-
-        client = MlflowClient()
-        client.set_registered_model_tag("tracking-readmitted-Decision Tree", "task", "classification")
 
 preprocess_data_task = PythonOperator(
     task_id='preprocess_data',
